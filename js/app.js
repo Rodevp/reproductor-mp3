4bd7437f279cbc1b064c053c6ae8cc9d3a35ba27
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-15 10:31:00 -0500

    arreglando bug de local storage y reproductor controls funcional

--- js/app.js
+++ js/app.js
@@ -12,32 +12,34 @@ const listAudios = [
     './audio/boca_yo_te_amo.mp3'
 ]
 
-
-
 const playing = document.getElementById('playing')
 const nextAudio = document.getElementById('next_audio')
 const prevAudio = document.getElementById('arrow__prev')
-const currentAudio = globalAudio()
-currentAudio.src = localStorage.getItem('currentAudio') !== null
-                        ? listAudios[listAudios.indexOf('currentAudio') ]
-                        : listAudios[0]
+const currentAudio = globalAudio(listAudios)
 
 console.log(localStorage.getItem('currentAudio'), currentAudio)
 
 playing.addEventListener('click', e => {
     
     if (!currentAudio.paused) {
+        console.log('he entrado al pause')
         pause(currentAudio)
-    }
-
-    if (currentAudio.paused) {
-        play(currentAudio)
+        
         localStorage.setItem('currentAudio', 
             localStorage.getItem('currentAudio') !== null
-                ? listAudios[listAudios.indexOf('currentAudio') ]
+                ? listAudios[listAudios.indexOf(localStorage.getItem('currentAudio')) ]
                 : listAudios[0]
         )
 
+    } else {
+        console.log('he entrado al play')
+        play(currentAudio)
+        
+        localStorage.setItem('currentAudio', 
+            localStorage.getItem('currentAudio') !== null
+                ? listAudios[listAudios.indexOf(localStorage.getItem('currentAudio')) ]
+                : listAudios[0]
+        )
     }
 
 })
--- js/audio.js
+++ js/audio.js
@@ -1,7 +1,10 @@
 const globalAudio = (list = []) => {
+    
+    
+    
     return new Audio(
         localStorage.getItem('currentAudio') !== null
-        ? list[list.indexOf('currentAudio') ]
+        ? list[list.indexOf(localStorage.getItem('currentAudio')) ]
         : list[0] )
 }
 
